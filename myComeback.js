0a6b9306b6778bc17ce77ad35e5bbfc4f0d23f76
Obehi Anetor Ibiezugbe <66558414+obehiibz@users.noreply.github.com>
2023-07-31 16:03:09 +0100

    git commit

---
+++ myComeback.js
@@ -0,0 +1,27 @@
+
+var myName;
+myName = 10;
+var ourName = 12;
+var a = 1;
+var b = 5;
+var c = "I am a";
+//var catName = "Oliver";
+//var catSound = "Meow!";
+let catName = "Olliver";
+let catSound = "Meow";
+console.log catName;
+window.alert("hello world");
+const FCC = "freeCodeCamp"; // Change this line
+let fact = "is cool!"; // Change this line
+fact = "is awesome!";
+console.log(FCC, fact); // Change this line
+const sum = 10 + 10;
+let myVar = 87;
+myVar++;
+let myVar = 11;
+// Only change code below this line
+myVar--
+const myDecimal = 5.7;
+const remainder = 11 % 3; //Set remainder equal to the remainder of 11 divided by 3 using the remainder (%) operator.
+
+
